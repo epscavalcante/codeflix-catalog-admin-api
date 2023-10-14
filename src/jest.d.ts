b7325a01a1be880d743b541177b53e96f9ed55c8
Eduardo Cavalcante <eduardo.ps.cavalcante@gmail.com>
2023-10-13 21:35:39 -0400

    chore: expect notification validation

--- src/jest.d.ts
+++ src/jest.d.ts
@@ -3,7 +3,8 @@ import { FieldErrors } from "./domain/validators/validator";
 declare global {
     namespace jest {
         interface Matchers<R> {
-            containsErrorMessage: (expected: FieldErrors) => R;
+            // containsErrorMessage: (expected: FieldErrors) => R;
+            notificationContainsErrorMessages: (expected: Array<string | {[key: string]: string[]}>) => R;
         }
     }
 }
