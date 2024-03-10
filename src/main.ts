bd050119be29151462f5d5addba44e37bb27c689
Eduardo Cavalcante <eduardo.ps.cavalcante@gmail.com>
2024-03-10 18:40:40 -0400

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { applyGlobalConfig } from './app.config.global';
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
 
+    app.enableCors();
     applyGlobalConfig(app);
 
     await app.listen(3000);
